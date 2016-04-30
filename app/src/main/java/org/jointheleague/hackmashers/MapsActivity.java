90b6f9ad5350a753bfb5439307b9888f2fbeec4b
Ryan N <ryguyn@gmail.com>
2016-04-30 12:17:52 -0700

    The app now uses the Google directions web API and draws the route on the map. Routes from current location to search location for walking.

--- app/src/main/java/org/jointheleague/hackmashers/MapsActivity.java
+++ app/src/main/java/org/jointheleague/hackmashers/MapsActivity.java
@@ -7,11 +7,13 @@ import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -27,19 +29,43 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, PlaceSelectionListener {
 
-    public final static int REQUEST_CODE_LOCATION = 1;
+    public static final int REQUEST_CODE_LOCATION = 1;
     public static final int DEFAULT_ZOOM = 15;
+    public static final int ZOOM_PADDING_PIXELS = 50;
+    public static final String DIRECTIONS_API = "https://maps.googleapis.com/maps/api/directions/json?";
+
+    public final static String DRIVING = "driving";
+    public final static String WALKING = "walking";
+    public final static String BICYCLING = "bicycling";
+    public final static String TRANSIT = "transit";
 
     private GoogleMap googleMap;
     private GoogleApiClient googleApiClient;//Unused right now
     private LocationManager locationManager;
     private PlaceAutocompleteFragment autocompleteFragment;
+    private Polyline currentPath;
+    private Place searchedPlace;
+    private GetDirections getDirectionsTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,19 +77,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .addApi(Places.GEO_DATA_API)
                 .addApi(Places.PLACE_DETECTION_API)
                 .build();
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);//TODO Use the fused location provider instead.
         autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
         autocompleteFragment.setHint(getString(R.string.search_hint));
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                pinSearchResult(place);
-            }
-
-            @Override
-            public void onError(Status status) {
-            }
-        });
+        autocompleteFragment.setOnPlaceSelectedListener(this);
         makeSearchBarPretty();
     }
 
@@ -124,14 +141,49 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void pinSearchResult(Place place) {
         googleMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName().toString())/*.icon(BitmapDescriptorFactory.fromResource(R.mipmap.green_dot)).anchor(0.5f, 0.5f).infoWindowAnchor(0.5f, 0.5f)*/);
+        zoomToPlaceAndCurrentLocation(place);
+    }
+
+    private void zoomToPlaceAndCurrentLocation(Place place) {
         CameraUpdate cameraUpdate;
         if (place.getViewport() != null) {
-            cameraUpdate = CameraUpdateFactory.newLatLngBounds(place.getViewport(), 25);
+            LatLngBounds bounds = place.getViewport().including(getLastKnownLatLng());
+            cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, ZOOM_PADDING_PIXELS);
         } else {
-            cameraUpdate = CameraUpdateFactory.newLatLngZoom(place.getLatLng(), DEFAULT_ZOOM);
+            LatLngBounds bounds = new LatLngBounds.Builder().include(place.getLatLng()).include(getLastKnownLatLng()).build();
+            cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, ZOOM_PADDING_PIXELS);
         }
         googleMap.animateCamera(cameraUpdate);
     }
+
+    private LatLng getLastKnownLatLng() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            return new LatLng(location.getLatitude(), location.getLongitude());
+        }
+        return null;
+    }
+
+    @Override
+    public void onPlaceSelected(Place place) {
+        searchedPlace = place;
+        pinSearchResult(place);
+        LatLng startCoordinates = null;//Temporary for testing
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startCoordinates = getLastKnownLatLng();
+        }
+        if (getDirectionsTask != null) {
+            getDirectionsTask.cancel(true);//TODO Make sure this doesn't break everything
+        }
+        getDirectionsTask = new GetDirections();
+        getDirectionsTask.execute(startCoordinates, place.getLatLng(), WALKING);
+    }
+
+    @Override
+    public void onError(Status status) {
+
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -182,5 +234,124 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    class GetDirections extends AsyncTask<Object, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(Object... params) {
+
+            LatLng start = (LatLng) params[0];
+            LatLng destination = (LatLng) params[1];
+            String mode = (String) params[2];
+            try {
+                String url = getDirectionsURL(start, destination, mode);
+                Log.d("URL", url);
+                return getJsonFromURL(url);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject directions) {
+            try {
+                Log.d("JSON", directions.toString());
+                JSONObject routeJSON = directions.getJSONArray("routes").getJSONObject(0);
+                String encodedPolyline = routeJSON.getJSONObject("overview_polyline").getString("points");
+                ArrayList<LatLng> pointsList = decodePolyline(encodedPolyline);
+                LatLng[] points = pointsList.toArray(new LatLng[pointsList.size()]);
+                if (currentPath != null) {
+                    currentPath.remove();
+                }
+                currentPath = googleMap.addPolyline(new PolylineOptions().add(points).color(ContextCompat.getColor(MapsActivity.this, R.color.colorPrimary)));
+                JSONObject boundsJSON = routeJSON.getJSONObject("bounds");
+                JSONObject southwestJSON = boundsJSON.getJSONObject("southwest");
+                JSONObject northeastJSON = boundsJSON.getJSONObject("northeast");
+                LatLngBounds routeBounds = new LatLngBounds(new LatLng(southwestJSON.getDouble("lat"), southwestJSON.getDouble("lng")), new LatLng(northeastJSON.getDouble("lat"), northeastJSON.getDouble("lng")));
+                zoomToRoute(routeBounds);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void zoomToRoute(LatLngBounds routeBounds) {
+        CameraUpdate cameraUpdate;
+        if (searchedPlace.getViewport() != null) {
+            LatLngBounds placeBounds = searchedPlace.getViewport();
+            LatLngBounds bounds = routeBounds.including(placeBounds.southwest).including(placeBounds.northeast);
+            cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, ZOOM_PADDING_PIXELS);
+        } else {
+            LatLngBounds bounds = routeBounds.including(searchedPlace.getLatLng());
+            cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, ZOOM_PADDING_PIXELS);
+        }
+        googleMap.animateCamera(cameraUpdate);
+    }
+
+    private JSONObject getJsonFromURL(String urlString) throws IOException, JSONException {
+        try {
+            URL url = new URL(urlString);
+            URLConnection connection;
+            connection = url.openConnection();
+            connection.connect();
+            InputStream stream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+            String jsonText = builder.toString();
+            return new JSONObject(jsonText);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getDirectionsURL(LatLng start, LatLng destination, String mode) {
+        return DIRECTIONS_API + "origin=" + start.latitude + "," + start.longitude + "&destination=" + destination.latitude + "," + destination.longitude + "&mode=" + mode;
+    }
+
+    private ArrayList<LatLng> decodePolyline(String encoded) {
+
+        ArrayList<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng(((double) lat / 1E5), ((double) lng / 1E5));
+            poly.add(p);
+        }
+        return poly;
     }
 }
