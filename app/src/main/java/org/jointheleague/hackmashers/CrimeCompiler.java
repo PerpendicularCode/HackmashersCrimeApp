25dfc7e92906062decc05a3fe70dd71de52d1b2c
TJ Gascho <sunfreezfilms@gmail.com>
2016-05-21 14:04:26 -0700

    Created 2 classes, CrimePoint and CrimeCompiler to start the process of parsing CSV data

--- app/src/main/java/org/jointheleague/hackmashers/CrimeCompiler.java
+++ app/src/main/java/org/jointheleague/hackmashers/CrimeCompiler.java
@@ -4,4 +4,7 @@ package org.jointheleague.hackmashers;
  * Created by SunFreezFilms on 5/21/16.
  */
 public class CrimeCompiler {
+    public CrimeCompiler(String fileName){
+
+    }
 }
--- app/src/main/java/org/jointheleague/hackmashers/CrimePoint.java
+++ app/src/main/java/org/jointheleague/hackmashers/CrimePoint.java
@@ -4,4 +4,15 @@ package org.jointheleague.hackmashers;
  * Created by SunFreezFilms on 5/21/16.
  */
 public class CrimePoint {
+    double lattitude, longitude;
+    int crime;
+
+    public CrimePoint(String input) {
+        int split1Pos = input.indexOf(',');
+        int split2Pos = input.indexOf(':');
+        if(split1Pos!=-1){
+
+        }
+
+    }
 }
