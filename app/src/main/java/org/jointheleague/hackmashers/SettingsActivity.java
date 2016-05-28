b348ab5fa1464ceb0ceb953eafe05d95ed86e854
Revanty <league@WTS14.attlocal.net>
2016-05-28 13:14:44 -0700

    Fixed Major Bug in Setting Menu

--- app/src/main/java/org/jointheleague/hackmashers/SettingsActivity.java
+++ app/src/main/java/org/jointheleague/hackmashers/SettingsActivity.java
@@ -31,6 +31,9 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Switch heatmapSwitch = (Switch) findViewById(R.id.stch);
+
+        heatmapSwitch.setChecked(DataUtil.IS_HEATMAP);
+
         heatmapSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
@@ -42,13 +45,41 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-        SeekBar slider = (SeekBar) findViewById(R.id.seekBar);
+        final SeekBar slider = (SeekBar) findViewById(R.id.seekBar);
+        slider.setProgress((int)(DataUtil.HEATMAP_OPACITY * 100));
         slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (slider.getId() == seekBar.getId()) {
+                    if (fromUser) {
+                        float realProgress = progress;
+                        DataUtil.HEATMAP_OPACITY = realProgress / 100;
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final SeekBar slider2 = (SeekBar) findViewById(R.id.seekBar2);
+
+        slider2.setProgress(DataUtil.HEATMAP_RADIUS);
+
+        slider2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(slider2.getId() == seekBar.getId())
                 if (fromUser) {
                     float realProgress = progress;
-                    DataUtil.HEATMAP_OPACITY = realProgress / 100;
+                    DataUtil.HEATMAP_RADIUS = (int)(realProgress / 5) * 2 + 10;
                 }
             }
 
@@ -61,28 +92,9 @@ public class SettingsActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
+
         });
 
-        SeekBar slider2 = (SeekBar) findViewById(R.id.seekBar2);
-        slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(fromUser){
-                    float realProgress = progress;
-                    DataUtil.HEATMAP_RADIUS =((int) ((realProgress / 5) * 2f)) + 10;
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
 
 
     }
